ca1d1830a354d37149266ee829b890556e8d7466
Danrley <danrleyhenrique2010@hotmail.com>
2017-10-04 23:29:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
+package ifrnpoo;
 
-
-public class Contato {
-	private String nome;
-    private String telefone;
-  
-    public String getNome()
-    {
-     return nome;
-    }
-    public void setNome(String nome)
-    {
-     this.nome = nome;
-    }
-    public String getTelefone()
-    {
-      return telefone;
-    }
-    public void setTelefone(String telefone)
-    {
-      this.telefone = telefone;
-    }
+public class Main {
+	public static void main(String[] args)
+	{
+		AgendaUI tela = new AgendaUI();
+		tela.UI();
+		tela.Run();
+	}
 }
