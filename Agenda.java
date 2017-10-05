b419f058b08b83a2604c49a7f65ad18a6d7baddd
Danrley <danrleyhenrique2010@hotmail.com>
2017-10-04 23:30:49 -0300

    Update Agenda.java

--- Agenda.java
+++ Agenda.java
@@ -27,12 +27,49 @@ public class Agenda {
 		return null;
 	}
 	
-	public void Remover()
+	public int BuscarParaRemocao(String nome)
 	{
-		
+		for(int i = 0 ; i < quantidade ; i++)
+		{
+			if(contatos[i].getNome().equals(nome))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void RemoverUltimo()
+	{
+		contatos[quantidade-1].setNome(null);
+		contatos[quantidade-1].setTelefone(null);
+		quantidade--;
+	}
+	
+	public void Remover(int indice)
+	{
+		for(int i = quantidade - indice; i < quantidade; i++)
+		{
+			contatos[indice].setNome(contatos[indice+1].getNome());
+			contatos[indice].setTelefone(contatos[indice+1].getNome());
+			indice++;
+		}
+		RemoverUltimo();
 	}
 	
 	public int Quantidade()
+	{
+		return quantidade;
+	}
+	
+	public String getContato(int i)
+	{
+		String c;
+		c = contatos[i].getNome() + " " + contatos[i].getTelefone();
+		return c;
+	}
+	
+	public int getQuantidade()
 	{
 		return quantidade;
 	}
