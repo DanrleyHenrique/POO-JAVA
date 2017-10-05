5fa7bb2661154cc67947dbe573d0103a59986c8c
Danrley <danrleyhenrique2010@hotmail.com>
2017-10-04 23:30:30 -0300

    Update AgendaUI.java

--- AgendaUI.java
+++ AgendaUI.java
@@ -24,42 +24,79 @@ public class AgendaUI {
 			case 1:
 				inserirContato();
 				break;
+			case 3:
+				listarContato();
+				break;
 			case 2:
 				buscarContato();
 				break;
-			case 3:
-				removerContato();
+			case 4:
+				removerUltimoContato();
+				break;
+			case 5:
+				buscarContatoParaRemocao();
 				break;
 			}
 		}while(opcao != 0);
+		System.out.println("Fim de execução!");
 	}
 	
 	private void inserirContato()
 	{
-		System.out.print("Digite o nome: ");
-		String nome = sc.nextLine();
-		System.out.println("Digite o telefone");
-		String telefone = sc.nextLine();
 		Contato novo = new Contato();
+		System.out.println("Digite o nome: ");
+		String nome = sc.next();
+		System.out.println("Digite o telefone: ");
+		String telefone = sc.next();
 		novo.setNome(nome);
 		novo.setTelefone(telefone);
 		agenda.InserirContato(novo);	
 	}
 	
-	private void buscarContato()
+	private void listarContato() 
 	{
-		System.out.print("Digite o nome: ");
-		String nome = sc.nextLine();
+		for(int i = 0; i < agenda.Quantidade(); i++)
+		{
+			System.out.println(agenda.getContato(i));
+		}
+	}
+	
+	private Contato buscarContato()
+	{
+		System.out.println("Digite o nome: ");
+		String nome = sc.next();
 		Contato contato = agenda.Buscar(nome);
-		if(contato != null)
+		if(contato != null) 
 			System.out.println(contato);
 		else
 			System.out.println("Contato não encontrado");
+		return contato;
 	}
 	
-	private void removerContato() 
+	private void buscarContatoParaRemocao()
 	{
-		System.out.println("A ser implementado");
+		System.out.println("Digite o nome: ");
+		String nome = sc.next();
+		int i = agenda.BuscarParaRemocao(nome);
+		if(i != -1) 
+			agenda.Remover(i);
+		else
+			System.out.println("Contato não encontrado");
+		
+	}
+	
+	private void removerUltimoContato() 
+	{
+		if(agenda.getQuantidade() == 0) 
+		{
+			System.out.println("Agenda vazia");
+		}
+		else
+		{
+			System.out.println("Insira o nome do indivíduo");
+			agenda.RemoverUltimo();
+			System.out.println("Contato removido");
+		}
 	}
 	private void printMenu() 
 	{
@@ -68,7 +105,9 @@ public class AgendaUI {
 		System.out.println("  0 - Sair");
 		System.out.println("  1 - Inserir contato");
 		System.out.println("  2 - Buscar contato");
-		System.out.println("  3 - Remover contato");
-		System.out.println("\n  Digite um opçãoo");
+		System.out.println("  3 - Listar contato");
+		System.out.println("  4 - Remover ultimo contato");
+		System.out.println("  5 - Remover por nome");
+		System.out.println("\n  Digite um opção");
 	}
 }
